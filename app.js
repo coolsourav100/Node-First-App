8ae6ac4c6f3b75096ad98a4bc4ee459be28d8b0f
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-06-04 10:04:09 +0530

    Running server with maulti res page

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-  console.log(req)
-  console.log("SOurav")
+  const url = req.url
+  res.setHeader('Content-Type','text/html')
+  if(url==='/'){
+  res.write(`<html><body><h1>Welcome to the Node.Js Server</h1></body></html>`)
+  }
+  if(url==='/home'){
+    res.write(`<html><body><h1>Welcome home</h1></body></html>`)
+  }
+  if(url==='/about'){
+    res.write(`<html><body><h1>Welcome About us Page</h1></body></html>`)
+  }
+  if(url==='/node'){
+    res.write(`<html><body><h1>Welcome to node js Project</h1></body></html>`)
+  }
+  // res.end()
 })
 
 server.listen(4000)
