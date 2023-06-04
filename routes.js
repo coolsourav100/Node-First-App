8da492be265979ad1cd2b3d4ba0d2f2dd3ba7cf0
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-06-04 18:59:22 +0530

    Add routing and cleaning app.js file

---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const requestHandler =(req,res)=>{
+  let display = fs.readFileSync('requestedData.text')
+  const url = req.url
+  const method = req.method
+  res.setHeader('Content-Type','text/html')
+  if(url==='/'){
+  res.write(`<html><body><h1>Welcome to the Node.Js Server</h1><h3>${display}</h3><form action='/home' method='POST'><input type='text' name = 'message'/><button>Send</button></form></body></html>`)
+   return res.end()
+  }
+  if(url==='/home' && method==='POST'){
+    const body=[]
+    req.on('data',(chunk)=>{
+        console.log(chunk)
+        body.push(chunk)
+    })
+    let dummy
+    req.on('end',()=>{
+      const DataBody = Buffer.concat(body).toString()
+      dummy=DataBody.split('=')[1]
+      dummy = dummy.split('+').join(' ')
+      console.log(dummy)
+      fs.writeFile('requestedData.text',dummy ,(err)=>{
+        res.setHeader('Location','/')
+        res.statusCode = 302
+        return res.end()
+      })
+    })
+    // fs.writeFileSync('message.text',dummy)
+    // res.write(`<html><body><h1>Welcome home</h1></body></html>`)
+  }
+  if(url==='/about'){
+    res.write(`<html><body><h1>Welcome About us Page</h1></body></html>`)
+    return res.end()
+  }
+  if(url==='/node'){
+    res.write(`<html><body><h1>Welcome to node js Project</h1></body></html>`)
+    return res.end()
+  }
+  // res.end()
+}
+module.exports = requestHandler
